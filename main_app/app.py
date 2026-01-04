ffc1b708190cfc3ade71e2a565e5926b1e45a3b6
Ahmed-Ashraf-Marzouk <a.a.elghawas@gmail.com>
2026-01-05 00:08:49 +0300

    add new sentences to Algeria

--- main_app/app.py
+++ main_app/app.py
@@ -75,7 +75,7 @@ S3_CLIENT = _create_s3_client()
 # COUNTRIES & DIALECTS
 # ===============================
 
-AVAILABLE_COUNTRIES = ["Egypt", "Saudi Arabia", "Morocco", "Yemen", "Jordan", "Palestine"]
+AVAILABLE_COUNTRIES = ["Egypt", "Saudi Arabia", "Morocco", "Yemen", "Jordan", "Palestine", "Algeria"]
 
 COUNTRY_EMOJIS = {
     "dz": "🇩🇿",
